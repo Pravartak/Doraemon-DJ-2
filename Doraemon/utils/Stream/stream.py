39936b2cc7f5373a8833108a7a8655e28b3eda61
Pravartak <109897902+Pravartak@users.noreply.github.com>
2022-09-25 13:12:51 +0530

    Update stream.py

--- Doraemon/utils/Stream/stream.py
+++ Doraemon/utils/Stream/stream.py
@@ -6,7 +6,7 @@ from pyrogram.types import InlineKeyboardMarkup
 
 import config
 from Doraemon import Carbon, YouTube, app
-from Doraemon.core.call import Doraemon
+from Doraemon.core.call import Dora
 from Doraemon.misc import db
 from Doraemon.utils.database import (add_active_chat,
                                        add_active_video_chat,
@@ -40,7 +40,7 @@ async def stream(
         if not await is_video_allowed(chat_id):
             raise AssistantErr(_["play_7"])
     if forceplay:
-        await Doraemon.force_stop_stream(chat_id)
+        await Dora.force_stop_stream(chat_id)
     if streamtype == "playlist":
         msg = f"{_['playlist_16']}\n\n"
         count = 0
@@ -89,7 +89,7 @@ async def stream(
                     )
                 except:
                     raise AssistantErr(_["play_16"])
-                await Doraemon.join_call(
+                await Dora.join_call(
                     chat_id, original_chat_id, file_path, video=status
                 )
                 await put_queue(
@@ -120,7 +120,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await Doraemonbin(msg)
+            link = await Dorabin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
@@ -170,7 +170,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Doraemon.join_call(
+            await Dora.join_call(
                 chat_id, original_chat_id, file_path, video=status
             )
             await put_queue(
@@ -224,7 +224,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Doraemon.join_call(
+            await Dora.join_call(
                 chat_id, original_chat_id, file_path, video=None
             )
             await put_queue(
@@ -278,7 +278,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Doraemon.join_call(
+            await Dora.join_call(
                 chat_id, original_chat_id, file_path, video=status
             )
             await put_queue(
@@ -339,7 +339,7 @@ async def stream(
             n, file_path = await YouTube.video(link)
             if n == 0:
                 raise AssistantErr(_["str_3"])
-            await Doraemon.join_call(
+            await Dora.join_call(
                 chat_id, original_chat_id, file_path, video=status
             )
             await put_queue(
@@ -391,7 +391,7 @@ async def stream(
         else:
             if not forceplay:
                 db[chat_id] = []
-            await Doraemon.join_call(
+            await Dora.join_call(
                 chat_id,
                 original_chat_id,
                 link,
